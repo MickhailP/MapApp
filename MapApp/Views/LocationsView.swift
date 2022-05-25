ec0a61f8379eab5198ddf67ccbb99dfe12dc1cbb
MickhailP <mich_p@inbox.ru>
2022-05-25 19:25:40 +0300

    last

--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -13,6 +13,7 @@ struct LocationsView: View {
     
     @EnvironmentObject private var locationViewModel:  LocationsViewModel
     
+    let maxWidthForIpad: CGFloat = 700
     
     var body: some View {
         ZStack {
@@ -20,6 +21,7 @@ struct LocationsView: View {
             VStack(spacing: 0){
                 header
                     .padding()
+                    .frame(maxWidth: maxWidthForIpad)
                 Spacer()
                 locationStackView
                 .padding()
@@ -83,6 +85,9 @@ extension LocationsView {
                 if locationViewModel.mapLocation == location {
                     LocationPreviewView(location: location)
                         .shadow(color: .black.opacity(0.3), radius: 20)
+                        .padding()
+                        .frame(maxWidth: maxWidthForIpad)
+                        .frame(maxWidth: .infinity)
                         .transition(.asymmetric(
                             insertion: .move(edge: .trailing),
                             removal: .move(edge: .leading)))
